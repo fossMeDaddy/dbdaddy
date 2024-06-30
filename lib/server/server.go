7d28e53ccbab4fc8f383f7c519faebd3113d8b9e
Kuvam Bhardwaj <kuvambhardwaj0529@gmail.com>
2024-06-30 18:09:46 +0530

    add cors to server

--- lib/server/server.go
+++ lib/server/server.go
@@ -1,9 +1,14 @@
 package libServer
 
-import "github.com/gofiber/fiber/v2"
+import (
+	"github.com/gofiber/fiber/v2"
+	"github.com/gofiber/fiber/v2/middleware/cors"
+)
 
 func StartServer() error {
 	app := fiber.New()
+
+	app.Use(cors.New())
 
 	defineHandlers(app)
 
