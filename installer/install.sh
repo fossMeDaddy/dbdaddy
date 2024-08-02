726716975060ce1e86576ffa4ed4e71b58543909
Kuvam Bhardwaj <kuvambhardwaj0529@gmail.com>
2024-08-02 19:12:06 +0530

    add install script for unix & linux systems

---
+++ installer/install.sh
@@ -0,0 +1,26 @@
+#!/bin/sh
+
+# RUNS STANDALONE
+
+pkg_bin_dir="$HOME/.dbdaddy/bin"
+pkg_bin="$pkg_bin_dir/dbdaddy"
+
+pkg_version="$(curl --silent https://raw.githubusercontent.com/fossMeDaddy/dbdaddy/main/version)"
+
+mkdir $pkg_bin_dir
+
+curl https://github.com/fossMeDaddy/dbdaddy/releases/download/$pkg_version/dbdaddy -L -o $pkg_bin
+chmod +x $pkg_bin
+
+for file in "$HOME/.bashrc" "$HOME/.zshrc" "$HOME/.profile" "$HOME/.zshenv"; do
+    if [ -f "$file" ]; then
+        if grep -q "alias dbdaddy=" "$file"; then
+            echo "Alias 'dbdaddy' already exists in $file. Skipping."
+        else
+            echo "\nalias dbdaddy=$pkg_bin" >> "$file"
+            echo "Appended alias to $file"
+        fi
+    fi
+done
+
+echo "Please restart terminal to run 'dbdaddy' globally."
