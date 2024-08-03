86a80cbbc7758671f1454154a48b4e1e40a34e93
Kuvam Bhardwaj <kuvambhardwaj0529@gmail.com>
2024-08-03 13:20:03 +0530

    CHECKPOINT: build-scripts to target diff os & archs

--- build-scripts/utils/utils.go
+++ build-scripts/utils/utils.go
@@ -5,6 +5,7 @@ import (
 	"os"
 	"os/exec"
 	"path"
+	"strings"
 )
 
 func GetCurrentVersion() string {
@@ -16,23 +17,34 @@ func GetCurrentVersion() string {
 	return string(r)
 }
 
-func GetOutFilePath() string {
-	outFile := path.Join("bin", "dbdaddy")
+func GetOutFilePath(goos string, goarch string) string {
+	outFile := path.Join("bin", fmt.Sprintf("dbdaddy-%s-%s", goos, goarch))
 
 	return outFile
 }
 
-func Build(outFile string) {
+func Build(goos string, goarch string) {
+	fmt.Println("Starting build for", goos, goarch)
+
+	GOOS := strings.ToLower(goos)
+	GOARCH := strings.ToLower(goarch)
+
+	outFile := path.Join("bin", fmt.Sprintf("dbdaddy-%s-%s", GOOS, GOARCH))
+
 	cmd := exec.Command("go", "build", "-o", outFile, ".")
+	cmd.Env = append(os.Environ(), "GOOS="+GOOS, "GOARCH="+GOARCH)
 	cmdErr := cmd.Run()
 	if cmdErr != nil {
-		fmt.Println(cmdErr)
+		fmt.Println("ERR:", cmdErr)
 		panic("error occured while running 'go build'")
 	}
+
+	fmt.Println("Built binary for", goos, goarch)
+	fmt.Println()
 }
 
-func Release(version string, file string) {
-	ghCmd := exec.Command("gh", "release", "create", version, file)
+func Release(version string, dir string) {
+	ghCmd := exec.Command("gh", "release", "create", version, dir)
 	ghCmd.Stdout = os.Stdout
 	ghCmd.Stdin = os.Stdin
 	ghCmd.Stderr = os.Stderr
