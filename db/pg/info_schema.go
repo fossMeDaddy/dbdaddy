969043723ad6b982c52505d04ac99a58b327fa31
Kuvam Bhardwaj <kuvambhardwaj0529@gmail.com>
2024-08-05 19:02:41 +0530

    minor refactor for SQL queries

--- db/pg/info_schema.go
+++ db/pg/info_schema.go
@@ -3,6 +3,7 @@ package pg
 import (
 	constants "dbdaddy/const"
 	"dbdaddy/db"
+	pg_queries "dbdaddy/db/pg/queries"
 	"dbdaddy/types"
 	"fmt"
 
@@ -35,78 +36,10 @@ func GetTableSchema(schema string, tablename string) (types.TableSchema, error) 
 	currBranch := viper.GetString(constants.DbConfigCurrentBranchKey)
 
 	table := types.TableSchema{
-		Db:     currBranch,
-		Schema: schema,
-		Name:   tablename,
+		Db: currBranch,
 	}
 
-	rows, err := db.DB.Query(fmt.Sprintf(`
-		select
-			infcol.column_name as column_name,
-			CASE
-				WHEN infcol.column_default IS NULL then '<null>'
-				ELSE infcol.column_default
-			END as default_value,
-			CASE
-				WHEN infcol.is_nullable = 'YES' THEN TRUE
-				ELSE FALSE
-			END AS nullable,
-			infcol.udt_name as datatype,
-			CASE
-				WHEN relcon.contype = 'p' then true
-				ELSE false
-			END as is_primary_key,
-			CASE
-				WHEN relcon.contype = 'f' then true
-				ELSE false
-			END as is_relation,
-			CASE
-				WHEN f_relfcol.table_schema IS NULL then ''
-				ELSE f_relfcol.table_schema
-			END as foreign_table_schema,
-			CASE
-				WHEN f_relfcol.table_name IS NULL then ''
-				ELSE f_relfcol.table_name
-			END as foreign_table_name,
-			CASE
-				WHEN f_relfcol.column_name IS NULL then ''
-				ELSE f_relfcol.column_name
-			END as foreign_column_name
-		from information_schema.columns infcol
-
-		inner join pg_namespace as relnsp on infcol.table_schema = relnsp.nspname
-		inner join pg_class as relcls on
-			relcls.relname = infcol.table_name and
-			relcls.relnamespace = relnsp.oid
-		left join pg_constraint as relcon on
-			relcon.conrelid = relcls.oid and
-			relcon.connamespace = relnsp.oid and
-			relcon.conkey[1] = infcol.ordinal_position and
-			(relcon.contype = 'p' OR relcon.contype = 'f')
-
-
-		-- FOREIGN KEYS START --
-		left join pg_class as f_relcls on relcon.conrelid = f_relcls.oid
-		left join pg_namespace as f_relnsp on relcon.connamespace = f_relnsp.oid
-
-		left join pg_class as f_relfcls on relcon.confrelid = f_relfcls.oid
-		left join pg_namespace as f_relfnsp on relcon.connamespace = f_relfnsp.oid
-
-		left join information_schema.columns as f_relcol on
-			f_relcol.table_schema = f_relnsp.nspname and
-			f_relcol.table_name = f_relcls.relname and
-			f_relcol.ordinal_position = relcon.conkey[1]
-		left join information_schema.columns as f_relfcol on
-			f_relfcol.table_schema = f_relfnsp.nspname and
-			f_relfcol.table_name = f_relfcls.relname and
-			f_relfcol.ordinal_position = relcon.confkey[1]
-		-- FOREIGN KEYS END --
-
-		where
-			infcol.table_schema not in ('pg_catalog', 'information_schema')	and
-			infcol.table_schema = '%s' and
-			infcol.table_name = '%s'
-	`, schema, tablename))
+	rows, err := db.DB.Query(pg_queries.QGetTableSchema(schema, tablename))
 	if err != nil {
 		return table, err
 	}
@@ -114,6 +47,8 @@ func GetTableSchema(schema string, tablename string) (types.TableSchema, error) 
 	for rows.Next() {
 		column := types.Column{}
 		if err := rows.Scan(
+			&table.Schema,
+			&table.Name,
 			&column.Name,
 			&column.Default,
 			&column.Nullable,
@@ -129,6 +64,8 @@ func GetTableSchema(schema string, tablename string) (types.TableSchema, error) 
 
 		table.Columns = append(table.Columns, column)
 	}
+
+	fmt.Println(table)
 
 	return table, nil
 }
--- db/pg/queries/info_schema.go
+++ db/pg/queries/info_schema.go
@@ -1,8 +1,13 @@
+// not necessary to move ALL SQL queries here
 package pg_queries
 
-var QGetTableSchema = func() string {
-	return `
+import "fmt"
+
+var QGetTableSchema = func(schema string, table string) string {
+	return fmt.Sprintf(`
         select
+            infcol.table_schema,
+            infcol.table_name,
             infcol.column_name as column_name,
             CASE
                 WHEN infcol.column_default IS NULL then '<null>'
@@ -67,5 +72,75 @@ var QGetTableSchema = func() string {
             infcol.table_schema not in ('pg_catalog', 'information_schema')	and
             infcol.table_schema = '%s' and
             infcol.table_name = '%s'
+    `, schema, table)
+}
+
+var QGetSchema = func() string {
+	return `
+        select
+            infcol.table_schema,
+            infcol.table_name,
+            infcol.column_name as column_name,
+            CASE
+                WHEN infcol.column_default IS NULL then '<null>'
+                ELSE infcol.column_default
+            END as default_value,
+            CASE
+                WHEN infcol.is_nullable = 'YES' THEN TRUE
+                ELSE FALSE
+            END AS nullable,
+            infcol.udt_name as datatype,
+            CASE
+                WHEN relcon.contype = 'p' then true
+                ELSE false
+            END as is_primary_key,
+            CASE
+                WHEN relcon.contype = 'f' then true
+                ELSE false
+            END as is_relation,
+            CASE
+                WHEN f_relfcol.table_schema IS NULL then ''
+                ELSE f_relfcol.table_schema
+            END as foreign_table_schema,
+            CASE
+                WHEN f_relfcol.table_name IS NULL then ''
+                ELSE f_relfcol.table_name
+            END as foreign_table_name,
+            CASE
+                WHEN f_relfcol.column_name IS NULL then ''
+                ELSE f_relfcol.column_name
+            END as foreign_column_name
+        from information_schema.columns infcol
+
+        inner join pg_namespace as relnsp on infcol.table_schema = relnsp.nspname
+        inner join pg_class as relcls on
+            relcls.relname = infcol.table_name and
+            relcls.relnamespace = relnsp.oid
+        left join pg_constraint as relcon on
+            relcon.conrelid = relcls.oid and
+            relcon.connamespace = relnsp.oid and
+            relcon.conkey[1] = infcol.ordinal_position and
+            (relcon.contype = 'p' OR relcon.contype = 'f')
+
+
+        -- FOREIGN KEYS START --
+        left join pg_class as f_relcls on relcon.conrelid = f_relcls.oid
+        left join pg_namespace as f_relnsp on relcon.connamespace = f_relnsp.oid
+
+        left join pg_class as f_relfcls on relcon.confrelid = f_relfcls.oid
+        left join pg_namespace as f_relfnsp on relcon.connamespace = f_relfnsp.oid
+
+        left join information_schema.columns as f_relcol on
+            f_relcol.table_schema = f_relnsp.nspname and
+            f_relcol.table_name = f_relcls.relname and
+            f_relcol.ordinal_position = relcon.conkey[1]
+        left join information_schema.columns as f_relfcol on
+            f_relfcol.table_schema = f_relfnsp.nspname and
+            f_relfcol.table_name = f_relfcls.relname and
+            f_relfcol.ordinal_position = relcon.confkey[1]
+        -- FOREIGN KEYS END --
+
+        where
+            infcol.table_schema not in ('pg_catalog', 'information_schema')
     `
 }
